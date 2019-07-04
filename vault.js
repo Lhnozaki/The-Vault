a94a06a7a479abce8ac04fcdfc23bc7c87e1f031
Lewis Nozaki <lhnozaki@gmail.com>
2019-07-03 19:32:20 -1000

    passed

--- vault.js
+++ vault.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = function() {
-  let vault = {};
+  let myVault = {};
   
   let setValue = function(key, value) {
-    vault[key] = value;
+    myVault[key] = value;
   }
   
   let getValue = function(key) {
-    if(vault.hasOwnProperty(key) === true) {
-      return vault[key];
+    if(myVault.hasOwnProperty(key) === true) {
+      return myVault[key];
     } else {
       return null;
     }
